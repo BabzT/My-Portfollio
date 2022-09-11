c7459cde8a087ffabbf4d307df17312cdd5e2908
BabzT <babatumdehezekiah7@gmail.com>
2022-09-11 16:02:20 +0100

    un

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueLazyLoad from 'vue3-lazyload'
 import './index.css'
 
-createApp(App).use(VueLazyLoad,{}).mount('#app')
+createApp(App).mount('#app')
